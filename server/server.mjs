69bd86ff3aaaa287ff8feb4c05e52ea8b98e0167
Kyle J. Harms <kyle.harms@cornell.edu>
2025-03-24 09:24:08 -0400

    class 17 materials

---
+++ server/server.mjs
@@ -0,0 +1,32 @@
+import path from 'path';
+import { fileURLToPath } from 'url';
+import 'dotenv/config';
+import express from 'express';
+import cors from 'cors';
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const port = process.env.PORT || 8080;
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+
+// path to the public folder
+const publicPath = path.join(__dirname, 'public');
+
+// TODO: GET /index.html
+
+
+
+// TODO: 404 - not found
+
+
+
+// 500 - Any server error
+// app.use((err, req, res, next) => {
+//   return res.status(500).json({ error: err });
+// });
+
+// start server on port
+app.listen(port, () => {
+  console.log(`app listening on http://localhost:${port}/`);
+});
