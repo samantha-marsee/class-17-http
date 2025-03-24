a55c4a3265a9e14a30cf00fcb509be430b951f59
samantha-marsee <sam572@cornell.edu>
2025-03-24 15:14:37 +0000

    json in

--- server/server.mjs
+++ server/server.mjs
@@ -23,6 +23,30 @@ app.get('/styles/site.css', (req, res) =>{
 })
 
 
+app.get('/api/documents.json', (req, res) => {
+  res.json([{
+    name: "Apple",
+    category: "fruit",
+    price: 1.99,
+    quantity: 150,
+    supplier: "Organic Farms Inc",
+    organic: true,
+    country: "USA",
+    expiration: new Date("2023-12-15")
+  },
+  {
+    name: "Banana",
+    category: "fruit",
+    price: 0.59,
+    quantity: 200,
+    supplier: "Tropical Imports",
+    organic: false,
+    country: "Ecuador",
+    expiration: new Date("2023-12-10")
+  }]).status(200)
+})
+
+
 // TODO: 404 - not found
 app.use((req, res, next) =>{
   return res.status(404)
